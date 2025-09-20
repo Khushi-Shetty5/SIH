7ec0bb950f3e51fdfd0de7e60ecb97a3c18d3384
neerajparamkar <neerajparamkar@gmail.com>
2025-09-20 22:49:38 +0530

    Updating doctor auth

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { DoctorApp } from './doctor/DoctorNavigator';
-import AppNavigator from './src/AppNavigator';
+import LabApp from './src/AppNavigator';
 import LandingScreen from './components/LandingScreen';
+import AuthNavigator from './components/AuthNavigator';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +13,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Landing" component={LandingScreen} />
+        <Stack.Screen name="Auth" component={AuthNavigator} />
         <Stack.Screen name="Doctor" component={DoctorApp} />
-        <Stack.Screen name="Lab" component={AppNavigator} />
+        <Stack.Screen name="Lab" component={LabApp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
