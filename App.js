a137958b1be1353cbb731d9995e99381291694a7
Vikas N <vikassharadha2006@gmail.com>
2025-09-20 14:29:24 +0530

    Normalize line endings

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
+import StackNavigator from './hospital/ReceptionistWindow/navigation/StackNavigator';
+import AdminNavigator from "./hospital/AdminWindow/navigation/AdminNavigator";
+import { colors } from './theme/colors';
+
+const Tab = createBottomTabNavigator();
+const isAdmin = true;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello from React Native 🎉</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StatusBar style="light" backgroundColor={colors.primary} />
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              headerShown: false,
+              tabBarActiveTintColor: colors.primary,
+              tabBarInactiveTintColor: 'gray',
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+
+                if (route.name === 'Receptionist') {
+                  iconName = 'people-outline';
+                } else if (route.name === 'Admin') {
+                  iconName = 'settings-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+            })}
+          >
+            <Tab.Screen name="Receptionist" component={StackNavigator} />
+            {isAdmin && <Tab.Screen name="Admin" component={AdminNavigator} />}
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
